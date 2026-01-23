3794ea1f2c1909e46621c5f591b6ca4d63304d42
SupriyaJoshi2022-github <108784079+SupriyaJoshi2022-github@users.noreply.github.com>
2026-01-23 15:43:11 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,15 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World! Maven" );
+public class App {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins + Maven!");
+
+        int sum = add(5, 7);
+        System.out.println("Sum of 5 + 7 = " + sum);
     }
 }
