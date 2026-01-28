6e7bfe5f9b15f132e67e74450aaf579cae5ae14e
SupriyaJoshi2022-github <108784079+SupriyaJoshi2022-github@users.noreply.github.com>
2026-01-28 14:13:15 +0530

    version2

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,10 +2,7 @@ package com.example;
 
 public class App {
 
-    public static int add(int a, int b) {
-        return a + b;
-    }
-
+    
     public static void main(String[] args) {
         System.out.println("Hello Jenkins + Maven!");
 
